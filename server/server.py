1f5770cd0fdde26d1206fb5115c682f32241f726
Ray Gonzales <raygon8712@gmail.com>
2018-11-27 09:31:12 -0700

    added gitignore

--- server/server.py
+++ server/server.py
@@ -10,6 +10,20 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return jsonify({"test":'testing'})
+
+
+@app.route("/client", methods=["POST"])
+def add_client():
+    content = request.get_json()
+    firstName = content.get('first_name')
+    lastName = content.get('last_name')
+    new_client = Client(first_name=firstName, last_name=lastName)
+    db.session.add(new_client)
+    db.session.commit()
+    client = {}
+    client['first_name'] = new_client.first_name
+    client['last_name'] = new_client.last_name
+    return jsonify(client=client)
    
 
 if __name__ == '__main__':
